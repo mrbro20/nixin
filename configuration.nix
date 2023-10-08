2aa0002e31063a18b6471c7c017f59306afa5db6
mrbro <108069180+mrbro20@users.noreply.github.com>
2023-10-09 00:10:34 +0530

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -5,57 +5,101 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
   # Bootloader.
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
-    };
+      efiSysMountPoint = "/boot"; };
     grub = {
        efiSupport = true;
-       device = "nodev";
-    };
+       useOSProber = true;
+       device = "nodev"; };
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "mundus";
+    firewall = { enable = false; }; 
+    networkmanager = { enable = true; };
+  };
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_IN";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    packages=[ pkgs.terminus_font ];
+    useXkbConfig = true; # use xkbOptions in tty.
+  };
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    nano
+    brave
+    wget
+    neofetch
+    gnome.nautilus
+    gnome.eog
+    gnome.gedit
+    gnome.gnome-tweaks
+    gnome.gnome-disk-utility
+    gnome.gnome-terminal
+    gnome-browser-connector
+    docker
+    qemu
+    virt-manager
+    virt-viewer
+    distrobox
+    rsync
+    imagemagick
+    libreoffice-still
+    fzf
+    zip
+    unzip
+    unrar
+    p7zip
+    ntfs3g
+    dosfstools
+    git
+    curl
+    evince
+    ffmpeg
+    sxiv
+    mpv
+    emacs-nox
+    pass
+    vscodium
+    zsh-syntax-highlighting
+    zsh-autosuggestions
+  ];
+
+  fonts = {
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      font-awesome
+      source-han-sans
+      source-han-sans-japanese
+      source-han-serif-japanese
+      (nerdfonts.override { fonts = [ "Meslo" ]; }) ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+        serif = [ "Noto Serif" "Source Han Serif" ];
+        sansSerif = [ "Noto Sans" "Source Han Sans" ]; };
+    };
   };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.core-utilities.enable = false;
 
   # Configure keymap in X11
   services.xserver = {
@@ -63,8 +107,11 @@
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # Enable Virtualisation support
+  virtualisation = {
+    docker = { enable = true; };
+    libvirtd = { enable = true; };
+  };
 
   # Enable sound with pipewire.
   sound.enable = true;
@@ -75,57 +122,67 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.error = {
-    isNormalUser = true;
-    description = "mrbro";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-    #  thunderbird
-    ];
+  services = {
+    # Enable the OpenSSH daemon.
+    openssh = {
+      enable = true;
+      allowSFTP = true; };
+    sshd.enable = true;
+    #printing.enable = true; 
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  programs.zsh.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.error = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" ];
+    shell = pkgs.zsh;
+  };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Nix daemon config
+  nix = {
+    # Flakes settings
+    package = pkgs.nixVersions.stable;
 
-  # List services that you want to enable:
+    settings = {
+      # Automate `nix store --optimise`
+      auto-optimise-store = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+      # Required by Cachix to be used as non-root user
+      trusted-users = [ "root" "error" ];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+      experimental-features = [ "nix-command" "flakes" ];
+      warn-dirty = false;
 
+      # Binary caches
+      #substituters = [
+      #  "https://cache.nixos.org"
+      #  "https://cache.garnix.io"
+      #  "https://gvolpe-nixos.cachix.org"
+      #];
+      #trusted-public-keys = [
+      #  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      #  "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      #  "gvolpe-nixos.cachix.org-1:0MPlBIMwYmrNqoEaYTox15Ds2t1+3R+6Ycj0hZWMcL0="
+      #];
+
+      # Avoid unwanted garbage collection when using nix-direnv
+      keep-outputs = true;
+      keep-derivations = true;
+    };
+  };
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
+  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
